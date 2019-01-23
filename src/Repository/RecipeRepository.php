4c1ef80c3fac9af5773d823d1a52a810adf8a5c4
ikuzmanovic <igor.kuzmanovic.996@gmail.com>
2019-01-23 11:11:00 +0100

    Finished recipe filtering

--- src/Repository/RecipeRepository.php
+++ src/Repository/RecipeRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Recipe;
+use App\Entity\Tag;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Component\Validator\Constraints\DateTime;
@@ -19,31 +20,39 @@ class RecipeRepository extends ServiceEntityRepository
      * @return array
      *
      * example: 'http://localhost:8000/api/recipes?filter[date]=2019-01-01&filter[category]=1,3,5&filter[tag]=2,4'
-     *
      */
     public function findByFilter(array $filter)
     {
-        $recipes = [];
-
-        $qb = $this->createQueryBuilder('r');
-        $qb->select(array('r'))
-            ->from('Recipe', 'r');
+        $qb = $this->createQueryBuilder('r')
+            ->addSelect('i, t')
+            ->join('r.ingredients', 'i')
+            ->join('r.tags', 't1')
+            ->join('r.tags', 't');
 
         if (key_exists('date', $filter))
         {
             $date = $filter['date'];
+
+            $qb->orWhere('r.date >= :date')
+                ->setParameter('date', $date);
         }
 
         if (key_exists('category', $filter))
         {
-            $categories = $filter['category'];
+            $categories = explode(',', $filter['category']);
+
+            $qb->orWhere('r.category IN (:categories)')
+                ->setParameter('categories', $categories);
         }
 
-        if (key_exists('tag', $filter))
+        if (key_exists('tags', $filter))
         {
-            $tags = $filter['tag'];
+            $tags = explode(',', $filter['tags']);
+
+            $qb->orWhere('t1 IN (:tags)')
+                ->setParameter('tags', $tags);
         }
 
-        return $recipes;
+        return $qb->getQuery()->getResult();
     }
 }
